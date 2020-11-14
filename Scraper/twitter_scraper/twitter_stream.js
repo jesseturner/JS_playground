956182d3cab9e1949779b1a952f2942ec16efe46
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-11-14 16:57:08 -0700

    Gets the full text, if needed. Will add this functionality to bot.js

--- Scraper/twitter_scraper/twitter_stream.js
+++ Scraper/twitter_scraper/twitter_stream.js
@@ -5,8 +5,13 @@ var config = require('./config');
 var T = new Twit(config);
 
 
-var stream = T.stream('statuses/filter', { track: '#ufc' });
+var stream = T.stream('statuses/filter', { track: 'ufc' });
 
 stream.on('tweet', function (tweet) {
-	console.log(tweet.text);
+	if (typeof tweet.extended_tweet !== 'undefined') {
+		console.log(tweet.extended_tweet.full_text); //if not undefined
+	}
+	else {
+		console.log(tweet.text);
+	}
 });
