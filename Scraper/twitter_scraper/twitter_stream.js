f51f317e3d78da3ccd64edbbe65fe0b5f3456fee
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-11-18 21:50:01 -0700

    Added variable. Most clean version.

--- Scraper/twitter_scraper/twitter_stream.js
+++ Scraper/twitter_scraper/twitter_stream.js
@@ -1,15 +1,15 @@
 var Twit = require('twit');
-
 var config = require('./config');
 
 var T = new Twit(config);
 
+var keyword = 'ufc';
 
-var stream = T.stream('statuses/filter', { track: 'ufc' });
+var stream = T.stream('statuses/filter', { track: keyword });
 
 stream.on('tweet', function (tweet) {
 	if (typeof tweet.extended_tweet !== 'undefined') {
-		console.log(tweet.extended_tweet.full_text); //if not undefined
+		console.log(tweet.extended_tweet.full_text); 
 	}
 	else {
 		console.log(tweet.text);
